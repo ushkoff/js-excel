10b4e8d84a157d19664f2dffd8d138d7de822150
ushkoff <lol454ushkbeavise@gmail.com>
2024-07-06 09:57:54 +0300

    Create unit testing env

---
+++ src/core/routes/router.spec.js
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+import {Router} from './Router'
+import {Page} from '../Page'
+
+class DashboardPage extends Page {
+    getRoot() {
+        const root = document.createElement('div')
+        root.innerHTML = 'dashboard'
+        return root
+    }
+}
+class ExcelPage extends Page {}
+
+describe('Router:', () => {
+    let router
+    let $root
+
+    beforeEach(() => {
+        $root = document.createElement('div')
+        router = new Router($root, {
+            dashboard: DashboardPage,
+            excel: ExcelPage
+        })
+    })
+
+    test('Router should be defined', () => {
+        expect(router).toBeDefined()
+    })
+
+    test('Router should render dashboard page', () => {
+        router.changePageHandler()
+        expect($root.innerHTML).toBe('<div>dashboard</div>')
+    })
+})
